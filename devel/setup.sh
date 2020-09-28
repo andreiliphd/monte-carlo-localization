d99249b4e251d892c1e48868584129fcf659901a
Andrei Li <andreiliphd@gmail.com>
2020-09-28 21:50:43 +0300

    Nodes are added

--- devel/setup.sh
+++ devel/setup.sh
@@ -11,7 +11,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/robond/Documents/monte-carlo-localization/devel}
+: ${_CATKIN_SETUP_DIR:=/home/andreiliphd/Documents/Udacity/RoboND/project_3/monte-carlo-localization/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
--- devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/robond/Documents/monte-carlo-localization/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/andreiliphd/Documents/Udacity/RoboND/project_3/monte-carlo-localization/src/ball_chaser/srv/DriveToTarget.srv")
