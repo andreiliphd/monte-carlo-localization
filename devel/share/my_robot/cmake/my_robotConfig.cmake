90167154a11cfd07d302fdee16c2bc54f633f2a3
Andrei Li <andreiliphd@gmail.com>
2020-09-29 00:05:18 +0300

    Hyperparameters tuning

--- devel/share/my_robot/cmake/my_robotConfig.cmake
+++ devel/share/my_robot/cmake/my_robotConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/andreiliphd/Documents/Udacity/RoboND/project_3/monte-carlo-localization/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/andreiliphd/Documents/Udacity/RoboND/project_3/monte-carlo-localization/devel/lib;/home/andreiliphd/Documents/Udacity/RoboND/project_3/monte-carlo-localization/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
